3e85ca1003f35250ccea577c8e79360b5a2111e6
Egemen <sucuogluegemen1@gmail.com>
2023-03-21 18:35:41 +0300

    görev tamamlandı

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
-import './less/index.less'
+import "./less/index.less";
 
-// Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+//Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın0
+//document.querySelector("h1").addEventListener("click",function(e){
+//alert("Bana tıkladın!")
+//});
 
+document.querySelectorAll("nav a").forEach((item) => {
+  item.addEventListener("mouseover", (item) => {
+    item.target.style.backgroundColor = "orange";
+  });
+});
 
-// Kodlar buraya gelecek!
+document.querySelectorAll("nav a").forEach((item) => {
+  item.addEventListener("mouseout", (item) => {
+    item.target.style.color = "green";
+  });
+});
+
+const imgOtobus = document.querySelector("img");
+imgOtobus.forEach((item) => {
+  item.addEventListener("click", (item) => {
+    item.target.setAttribute("style", "filter:grayscale(100%)");
+  });
+});
+
+document.querySelectorAll("img");
+imgOtobus.forEach((item) => {
+  item.addEventListener("dblclick", (item) => {
+    item.setAttribute("style", "filter:grayscale(0%)");
+  });
+});
+
+const acılıs = document.getElementById("login");
+window.addEventListener("load", (item) => {
+  setTimeout(() => {
+    acilis.style.display = "none";
+  }, 2000);
+});
+
+const imgwheel = document.querySelectorAll("footer");
+imgwheel.forEach((footer) => {
+  footer.addEventListener("wheel", (event) => {
+    footer.style.fontSize = "45";
+  });
+});
